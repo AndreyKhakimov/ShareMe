ca771a28db05da6058177fdbf6854d8c20f09c29
Andrey Khakimov <khakimov013@gmail.com>
2022-04-10 16:12:20 +0200

    Initial Commit

---
+++ ShareMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShareMe
+//
+//  Created by Andrey Khakimov on 10.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
